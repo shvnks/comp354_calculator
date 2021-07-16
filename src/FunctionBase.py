4a1f0772ecedc2c1ba35e9263905c20e870d723e
Arash Manpreet Singh <singharash09@gmail.com>
2021-07-16 17:20:58 -0400

    created class for my function and added PI to base

--- src/FunctionBase.py
+++ src/FunctionBase.py
@@ -7,6 +7,7 @@ class FunctionBase(ABC):
     def __init__(self) -> None:
         super(ABC, self).__init__()
         self.MAX_TERMS = 100
+        self.PI = 3.1415926535897932384626433
         pass
 
     # calculateEquation: Method that will be called to calculate the math function
--- src/FunctionSinh.py
+++ src/FunctionSinh.py
@@ -1,43 +1,60 @@
-from FunctionFactorial import FunctionFactorial
+from FunctionBase import FunctionBase
 
-#constants
-PI = 3.1415926535897932384626433
-MAX_RESULT = 10000000
-MIN_RESULT = -10000000
-#main funciton
-def sinh(num, isDeg=False):
-    if(isDeg):
-        num = num * PI/180
+
+class FunctionSinh(FunctionBase):
     
-    num1 = custom_exp(num)
-    num2 = custom_exp(-num)
+    #constants
+    MAX_RESULT = 10000000
+    MIN_RESULT = -10000000
 
-    result = (num1-num2)/2
+    #constructor
+    def __init__(self, num: float) -> None:
+        super(FunctionSinh, self).__init__()
+        self.num = num
 
-    if(result > MAX_RESULT):
-        raise Exception("result too large")
-    elif(result < MIN_RESULT):
+    # calculate sinh
+    # input: num (int), isDeg (boolean)
+    # output: sinh(num)
+    def calculateEquation(self, isDeg=False) -> float:
+
+        #if the number is in degrees, convert it to radians
+        if(isDeg):
+            self.num = self.num * self.PI/180
+            
+        #calculate sinh
+        num1 = self.__custom_exp(self.num)
+        num2 = self.__custom_exp(-self.num)
+        result = (num1-num2)/2
+        
+        #handle exceptions. We don't want a number too large or small
+        if(result > self.MAX_RESULT):
+            raise Exception("result too large")
+        elif(result < self.MIN_RESULT):
           raise Exception("result too small")      
+          
+        return round(result,5)
 
-    return result
-
-
-#helper functions
-def custom_factorial(n):
-    result=1
-    if n==0:
-        return 1
-    elif n>0:
-        for i in range(1,n+1):
-            result = result*i
+    # calculate factorial (private helper function)
+    # input: n (int)
+    # output: n!
+    def __custom_factorial(self,n:int):
+        result=1
+        if n==0:
+            return 1
+        
+        elif n>0:
+            for i in range(1,n+1):
+                result = result*i
         return result
-    
-
-def custom_exp(x):
-    n=0
-    for i in range(0, 100):
-      n= n + (x**i)/custom_factorial(i)
-    return n
 
 
-print(sinh(100))
+    # calculate e^x (private helper function) using Mclaurin Series
+    # input: x (int)
+    # output: e^x
+    def __custom_exp(self,x):
+        n=0
+        for i in range(0, self.MAX_TERMS):
+            n= n + (x**i)/(self.__custom_factorial(i))
+        return n
+
+
