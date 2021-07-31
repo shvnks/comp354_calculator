d504af0e60b5347f454517edf80295d206d8ab55
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-30 20:03:21 -0400

    Fixed comments

--- src/FunctionBase.py
+++ src/FunctionBase.py
@@ -1,22 +1,23 @@
 
 from abc import ABC, abstractmethod
 
-# FunctionBase: Base class used to for the custom math functions
 class FunctionBase(ABC):
-    #constructor: initialize the class, takes in the values that will be used to evaluate the function
+    '''Base class for all custom Math Functions'''
+
     def __init__(self) -> None:
+        '''Constructor. Initialize constants'''
         super(ABC, self).__init__()
         self.MAX_TERMS = 100
         self.PI = 3.1415926535897932384626433
         self.e = 2.7182818284590452353602874
         self.MAX_RESULT = 10000000
         self.MIN_RESULT = -10000000
-        pass
 
-    # calculateEquation: Method that will be called to calculate the math function
     @abstractmethod
     def calculateEquation(self) -> float:
+        '''Abstact function that will be used to calculate the math function'''
         raise NotImplementedError()
 
     def degreeToRadian(self, degreeValue):
+        '''Function used to convert degrees to radians for trig functions'''
         return degreeValue * self.PI/180
--- src/FunctionMAD.py
+++ src/FunctionMAD.py
@@ -4,25 +4,33 @@ from CalculationErrorException import CalculationErrorException
 
 
 class FunctionMad(FunctionBase):
+    '''Class used to calculate the Mean Average Deviation function.'''
 
-    # constructor
     def __init__(self, arguments: list) -> None:
+        '''Constructor.'''
         super(FunctionMad, self).__init__()
         self.arguments = arguments
 
-
-def calculateEquation(self) -> float:
-
-    # calculating the mean of inputs
-    mean = 0
-    for i in range(0, len(self.arguments)):
-        mean += self.arguments[i]
-    mean = mean/len(self.arguments)
-    deviation = 0
-    summ = 0
-    for i in range(0, len(self.arguments)):
-        deviation = self.arguments[i]-mean
-    if deviation < 0:
-        deviation = -deviation
-    summ += deviation
-    return summ / len(self.arguments)
+    def calculateEquation(self) -> float:
+        '''
+            Function used to calculate the Mean Absolute Deviation of the input values.
+            Returns MAD(self.arguments)
+        '''
+        # Calculate the mean of inputs
+        mean = 0
+        for i in range(0, len(self.arguments)):
+            mean += self.arguments[i]
+        mean = mean/len(self.arguments)
+        
+        deviation = 0
+        summ = 0
+        
+        # Calculate deviation
+        for i in range(0, len(self.arguments)):
+            deviation = self.arguments[i]-mean
+        if deviation < 0:
+            deviation = -deviation
+        
+        summ += deviation
+        
+        return summ / len(self.arguments)
