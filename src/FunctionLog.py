44085845e5aaa5425a93274cca14ac5659e50783
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-07-30 23:12:07 -0400

    Fix log function
    The previous implementation was only valid for x < 1, this one works for any x>0

--- src/FunctionLog.py
+++ src/FunctionLog.py
@@ -1,7 +1,7 @@
 from FunctionBase import FunctionBase
 from CalculationErrorException import CalculationErrorException
-from FunctionExponent import FunctionExponent
-from FunctionAbs import FunctionAbs
+import FunctionExponent
+import FunctionAbs
 
 class FunctionLog(FunctionBase):
     '''Class used to calculate the log function.'''
@@ -12,32 +12,21 @@ class FunctionLog(FunctionBase):
         self.b = b
         self.n = n
 
-    def ln(y: float) -> float:
-        '''Function used to calculate ln through Taylor Series'''
-
-        # x represents the exponent
-        x = y - 1
-
-        # i represents the starting integer for the riemann sum
-        i = 1
+    def ln(self, x: float) -> float:
+        '''
+            Function used to calculate ln through Series Expansion.
+            Where ln(x) = 2 * Sum_1^\infty{ (((x-1)/(x+1))^(2n-1)) / (2n-1) }
+        '''
 
         # Creating the sum variable
         sum = 0
 
         # Loop until the end condition is met
-        for k in range(self.MAX_TERMS):
-
-            # Adding the values
-            add = FunctionExponent(-1, k + 1).calculateEquation() * FunctionExponent(x, i).calculateEquation() / i
-
-            # Stop when the difference is smaller than 0.0000000001
-            if FunctionAbs(add).calculateEquation() < 0.0000000001:
-                break
+        for n in range(1, self.MAX_TERMS):
+            add = FunctionExponent.FunctionExponent((x-1)/(x+1), (2*n-1)).calculateEquation() / (2*n-1)
             sum += add
 
-            # Move to the next integer in the riemann sum
-            i += 1
-        return sum
+        return 2 * sum
 
     def calculateEquation(self) -> float:
         '''
@@ -49,7 +38,7 @@ class FunctionLog(FunctionBase):
         if self.n > 0 and self.b > 1:
 
             # Using log identity: log_b(n) = log_x(n) / log_x(b)
-            result = ln(self.n) / ln(self.b)
+            result = self.ln(self.n) / self.ln(self.b)
 
             # Handle exceptions for results that are too large or small
             if(result > self.MAX_RESULT):
@@ -60,3 +49,7 @@ class FunctionLog(FunctionBase):
             return result
         else:
             raise CalculationErrorException("Invalid Input: validate base and argument values.")
+
+
+if __name__ == '__main__':
+    print(FunctionLog(2, 4).calculateEquation())
