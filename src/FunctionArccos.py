f25f8a5091572173de9340c09ece629b1ea337c0
Will23062009 <71350630+Will23062009@users.noreply.github.com>
2021-07-30 20:34:59 -0400

    Updated the Arccos and Arcsin Functions
    Updated the Arccos and Arcsin Functions. Danny, let me know know if there's anything more.

--- src/FunctionArccos.py
+++ src/FunctionArccos.py
@@ -4,11 +4,24 @@ from FunctionArcsin import FunctionArcsin
 import math
 
 
-# FunctionArcsin: Class use to define and calculate Arcsin(x)
-class FunctionAcrcos(Functionbase):
+# FunctionArccos: Class use to define and calculate Arccos(x)
+class FunctionArccos(FunctionBase):
 
-        def my_arccos(x):
-            # To achieve Arccos(x) simply divide 2/pi - Arcsin(x)
-            my_pi = 3.141592653589793238
-            arccos = ((my_pi/2) - FunctionArcsin(FunctionBase))
-            return arccos
+     #constructor
+    def __init__(self, num: float) -> None:
+        super(FunctionArccos, self).__init__()
+        self.num = num
+    
+    # Calculate Arccos(x)
+    # Input: num (int), isDeg (boolean)
+    # Output: Arccos(num)
+    def calculateEquation(self, isDeg=False) -> float:
+
+        # If the number is in degrees, convert it to radians
+        if(isDeg):
+            self.num = self.num * self.PI/180
+   
+        # To achieve Arccos(x) simply divide 2/pi - Arcsin(x)
+        my_pi = 3.141592653589793238
+        arccos = ((my_pi/2) - FunctionArcsin(FunctionBase).calculateEquation())
+        return arccos
--- src/FunctionArcsin.py
+++ src/FunctionArcsin.py
@@ -5,19 +5,32 @@ from CalculationErrorException import CalculationErrorException
 import math
 
 # FunctionArcsin: Class use to define and calculate Arcsin(x)
-class FunctionAcrsin(Functionbase):
-    def my_arcsin(x):
-        var_x = float(x)
+class FunctionAcrsin(FunctionBase):
+    
+    #constructor
+    def __init__(self, num: float) -> None:
+        super(FunctionArcsin, self).__init__()
+        self.num = num
+
+    # calculate Arcsin
+    # input: num (int), isDeg (boolean)
+    # output: Arcsin(num)
+    def calculateEquation(self, isDeg=False) -> float:
+    
+        #if the number is in degrees, convert it to radians
+        if(isDeg):
+            self.num = self.num * self.PI/180
+    
         
-            # sum1 variable is the sum of the series.
+        # sum1 variable is the sum of the series.
         sum1 = 0
 
 	    # Current for loop to create the length of the series.
 	    # Note that the range is set to 130 since anything greater can cause an overflow for different IDEs. 
-        for n in range(0, 130):
+        for n in range(0, MAX_TERMS(FunctionBase).calculateEquation()):
 
-        # Formula of the Talor series in terms of the integration for arcsin
-            x1 = (var_x ** (2 * n + 1)) / (2 * n + 1) / float(2 ** (2 * n))
+            # Formula of the Talor series in terms of the integration for arcsin
+            x1 = (self.num ** (2 * n + 1)) / (2 * n + 1) / float(2 ** (2 * n))
             x2 = float(1)        
         
         
@@ -28,7 +41,7 @@ class FunctionAcrsin(Functionbase):
             for i in range(2, n + 1):
                 x2 /= i
 
-	    #The addition to the next series with the multiplication of the intergration.
+	        #The addition to the next series with the multiplication of the intergration.
             sum1 += x1 * x2
 
         return sum1
