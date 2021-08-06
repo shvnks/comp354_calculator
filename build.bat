d4dbb8192e90e214800e66bd3b8869ea80b3ae37
Danny Roux-Dufault <danny.roux@hotmail.fr>
2021-08-06 19:19:09 -0400

    Update run and add build

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+pyinstaller -F ^
+--paths="%~dp0\src" ^
+--paths="%~dp0\src\InterpreterModule" ^
+--specpath tmp ^
+--name ETERNITYCalculator ^
+--distpath "%~dp0\ETERNITYCalculator" ^
+-y --clean --noconsole src\main.py
+
+xcopy /Y "%~dp0\src\darkstyle.qss" "%~dp0\ETERNITYCalculator\"
